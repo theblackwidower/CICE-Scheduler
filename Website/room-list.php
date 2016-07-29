fc856443f68f4df2fbeafc4fc6f13d34bfa2eba7
theblackwidower <theblackwidower@noprestige.com>
2016-07-29 13:42:48 -0400

    Cleaned up CSS selectors
    Simplified the majority of CSS selectors.

--- Website/room-list.php
+++ Website/room-list.php
@@ -8,7 +8,7 @@ foreach ($all_campuses as $campus)
 {
 	echo '<div class="room_list">';
 	echo $campus['campus_name'];
-	echo '<ul class="room_list">';
+	echo '<ul>';
 	$all_rooms = get_all_rooms_on_campus($campus['campus_id']);
 	foreach ($all_rooms as $room)
 		echo display_room_li($room);
