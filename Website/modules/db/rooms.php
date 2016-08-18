f633de0a1987c9a5832f8febaf55a4b9526ec69c
theblackwidower <theblackwidower@noprestige.com>
2016-08-17 20:16:40 -0400

    Removed redundant code from room search
    Room search included a replace command, removing spaces from the room
    numbers in the database while searching. But room numbers shouldn't have
    any spaces in. Only letters and numbers. So, I removed the replace
    command. It was unneccessary.

--- Website/modules/db/rooms.php
+++ Website/modules/db/rooms.php
@@ -9,7 +9,7 @@ function search_rooms($search, $max_results = MAX_SEARCH_RESULT)
 {
 	global $conn;
 	$stmt = $conn->prepare("SELECT room_number, campus_name FROM tbl_rooms, tbl_campuses
-			WHERE REPLACE(room_number, ' ', '') ILIKE '%' || REPLACE(:search, ' ', '') || '%'
+			WHERE room_number ILIKE '%' || REPLACE(:search, ' ', '') || '%'
 			AND tbl_rooms.campus_id = tbl_campuses.campus_id
 			ORDER BY tbl_rooms.campus_id, room_number LIMIT :max_results");
 	$stmt->bindValue(':search', $search); //'%' is wildcard in PostgreSQL
